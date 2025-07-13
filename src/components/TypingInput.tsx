350f5558a4967a38ba92eb57cc8ffa29024a6fc4
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-07-13 13:03:12 +0530

    feat: Introduce a state to track backspace clicks style: Enhance styling for backspace triggers

--- src/components/TypingInput.tsx
+++ src/components/TypingInput.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import FeedbackPanel from './FeedbackPanel';
 
 type Mode = 'quotes' | 'paragraphs' | 'codes';
@@ -16,6 +16,12 @@ const caretColorMap: Record<Mode, string> = {
     codes: 'caret-blue-500',
 };
 
+const bkspRingColorMap: Record<Mode, string> = {
+    quotes: 'ring-red-500',
+    paragraphs: 'ring-green-500',
+    codes: 'ring-blue-500',
+};
+
 const fontMap: Record<Mode, string> = {
     quotes: 'font-serif',
     paragraphs: 'font-sans',
@@ -24,6 +30,7 @@ const fontMap: Record<Mode, string> = {
 
 const TypingInput = ({ value, onChange, mode, targetText, strict }: TypingInputProps) => {
     const inputRef = useRef<HTMLTextAreaElement>(null);
+    const [bkspClicked, setBkspClicked] = useState<boolean>(false);
     useEffect(() => {
         if (inputRef.current) inputRef.current?.focus();
     }, []);
@@ -41,14 +48,25 @@ const TypingInput = ({ value, onChange, mode, targetText, strict }: TypingInputP
     const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
         if (e?.key === 'Backspace' && strict) {
             e.preventDefault();
+            setBkspClicked(true);
+            setTimeout(() => {
+                setBkspClicked(false);
+            }, 500);
         }
     };
 
     return (
-        <div className="max-w-screen flex flex-col items-center">
+        <div className="relative max-w-screen flex flex-col items-center">
+            {strict && bkspClicked && (
+                <span className="absolute strict-pill px-1.5 py-0.5 bg-pink-50 rounded-full border border-pink-500 text-pink-500 shadow-sm text-sm">
+                    🚫 Backspace disabled
+                </span>
+            )}
             <textarea
                 rows={3}
-                className={`bg-transparent w-4xl h-full p-3 rounded-md resize-none border focus:outline-none focus:ring-blue-400 cursor-text ${caretColorMap[mode]} ${fontMap[mode]}`}
+                className={`bg-transparent w-4xl h-full p-3 rounded-md resize-none border focus:outline-none cursor-text transition-all duration-300 ${
+                    caretColorMap[mode]
+                } ${fontMap[mode]} ${bkspClicked ? `ring-2 ${bkspRingColorMap[mode]}` : ''}`}
                 value={value}
                 onChange={(e) => onChange(e.target.value)}
                 onKeyDown={handleKeyDown}
